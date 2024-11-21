8d4e9b7ec878e1c272a09fa70fd0b541499b40ab
ktunprasert <ktunprasert@outlook.com>
2024-11-21 20:03:16 +0000

    kindergarten: washed

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -1,25 +1,25 @@
 module KindergartenGarden
 
 type Plant =
-    | Grass = 'G'
-    | Clover = 'C'
-    | Radishes = 'R'
-    | Violets = 'V'
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
 
-let plantFromChar (c: char) =
-    match c with
-    | 'G' -> Plant.Grass
-    | 'C' -> Plant.Clover
-    | 'R' -> Plant.Radishes
-    | 'V' -> Plant.Violets
-    | _ -> failwith "Invalid plant"
+    static member Parse (c: char) =
+        match c with
+        | 'G' -> Grass
+        | 'C' -> Clover
+        | 'R' -> Radishes
+        | 'V' -> Violets
+        | _ -> failwith "Invalid plant"
 
 let plants (diagram: string) (student: string) : Plant list =
-    match diagram.Split('\n') with
+    match diagram.Split() with
     | [| row1; row2 |] ->
         let i = (int student.[0] - int 'A') * 2
-        let indexes = [ i; i + 1 ]
 
         [ row1; row2 ]
-        |> List.collect (fun row -> indexes |> List.map (fun index -> plantFromChar row.[index]))
+        |> List.collect (fun row -> row |> Seq.toList |> List.skip i |> List.take 2)
+        |> List.map Plant.Parse
     | _ -> failwith "Invalid diagram"
