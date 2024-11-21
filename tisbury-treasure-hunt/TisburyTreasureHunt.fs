9eb18877f618b174d19e7ab0c709a2c1f51993f2
ktunprasert <ktunprasert@outlook.com>
2024-11-21 18:57:31 +0000

    tisbury: done

---
+++ tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -0,0 +1,20 @@
+module TisburyTreasureHunt
+
+let getCoordinate (line: string * string) : string = snd line
+
+let convertCoordinate (coordinate: string) : int * char = int coordinate.[0] - 48, coordinate.[1]
+
+let compareRecords
+    ((_, azaraCoords): string * string)
+    ((_, ruisCoords, _): string * (int * char) * string)
+    : bool =
+    convertCoordinate azaraCoords = ruisCoords
+
+let createRecord (az: string * string) (ru: string * (int * char) * string) : (string * string * string * string) =
+    let azName, azCoords = az
+    let ruName, _, ruColor = ru
+
+    if compareRecords az ru then
+        azCoords, ruName, ruColor, azName
+    else
+        "", "", "", ""
