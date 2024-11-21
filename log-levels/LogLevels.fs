cbfeaa23651752ebe91733f290abe3fddb2c8366
ktunprasert <ktunprasert@outlook.com>
2024-11-21 18:34:40 +0000

    log-levels: washed

--- log-levels/LogLevels.fs
+++ log-levels/LogLevels.fs
@@ -1,22 +1,11 @@
 module LogLevels
 
-open System
-
 let message (logLine: string): string =
-    match logLine.Trim().Split(": ", StringSplitOptions.TrimEntries) with
-    | [|"[INFO]";message|] -> message
-    | [|"[WARNING]";message|] -> message
-    | [|"[ERROR]";message|] -> message
-    | _ -> failwith "Invalid log line"
-
+    logLine.Split(":").[1].Trim()
 
 let logLevel(logLine: string): string =
-    match logLine with
-    | s when s.StartsWith("[INFO]") -> "info"
-    | s when s.StartsWith("[WARNING]") -> "warning"
-    | s when s.StartsWith("[ERROR]") -> "error"
-    | _ -> failwith "Invalid log line"
+    logLine.Split(":").[0].Trim('[', ']').ToLower()
 
 
 let reformat(logLine: string): string =
-    message logLine + $" ({logLevel logLine})"
+    $"{message logLine} ({logLevel logLine})"
