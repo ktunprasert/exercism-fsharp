7ce776b0430be7a45323f199082c661b985f6cc1
ktunprasert <ktunprasert@outlook.com>
2024-11-21 18:41:06 +0000

    bob: washed

--- bob/Bob.fs
+++ bob/Bob.fs
@@ -1,14 +1,12 @@
 module Bob
 
+let isQuestion (input: string) : bool = input.Trim().EndsWith("?")
+let isYelling (input: string) : bool = input.ToUpper() = input && input.ToLower() <> input
+
 let response (input: string) : string =
-    if input.Trim().Length = 0 then
-        "Fine. Be that way!"
-    elif input.ToUpper() = input && input.ToLower() <> input then
-        if input.Trim().EndsWith("?") then
-            "Calm down, I know what I'm doing!"
-        else
-            "Whoa, chill out!"
-    elif input.Trim().EndsWith("?") then
-        "Sure."
-    else
-        "Whatever."
+    match input.Trim() with
+    | "" -> "Fine. Be that way!"
+    | _ when isYelling input && isQuestion input -> "Calm down, I know what I'm doing!"
+    | _ when isYelling input -> "Whoa, chill out!"
+    | _ when isQuestion input -> "Sure."
+    | _ -> "Whatever."
