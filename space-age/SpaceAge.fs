6b408520a857fa86d74be9ac9734d228f2a76684
ktunprasert <ktunprasert@outlook.com>
2024-11-21 19:36:30 +0000

    space-age: fix var name

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,6 +1,6 @@
 module SpaceAge
 
-let orbitalPeriod = 31557600.0
+let earthYearInSeconds = 31557600.0
 
 type Planet =
     | Mercury
@@ -22,6 +22,6 @@ let age (planet: Planet) (seconds: int64): float =
     | Saturn -> 29.447498
     | Uranus -> 84.016846
     | Neptune -> 164.79132
-    |> (*) orbitalPeriod
+    |> (*) earthYearInSeconds
     |> (/) (float seconds)
 
