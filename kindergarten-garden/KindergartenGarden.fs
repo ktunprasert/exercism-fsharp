113059d656ee15816c6ff1cdaad636000823cbb9
ktunprasert <ktunprasert@outlook.com>
2024-11-21 20:07:48 +0000

    kindergarten: mega wash

--- kindergarten-garden/KindergartenGarden.fs
+++ kindergarten-garden/KindergartenGarden.fs
@@ -15,11 +15,9 @@ type Plant =
         | _ -> failwith "Invalid plant"
 
 let plants (diagram: string) (student: string) : Plant list =
-    match diagram.Split() with
-    | [| row1; row2 |] ->
-        let i = (int student.[0] - int 'A') * 2
+    let i = (int student.[0] - int 'A') * 2
 
-        [ row1; row2 ]
-        |> List.collect (fun row -> row |> Seq.toList |> List.skip i |> List.take 2)
-        |> List.map Plant.Parse
-    | _ -> failwith "Invalid diagram"
+    diagram.Split()
+    |> Seq.collect (fun row -> row |> Seq.skip i |> Seq.take 2)
+    |> Seq.map Plant.Parse
+    |> Seq.toList
