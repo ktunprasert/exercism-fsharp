5dcee90db11ebf08230926012a0e8aa63b66419d
ktunprasert <ktunprasert@outlook.com>
2024-11-22 11:29:22 +0000

    yacht: washing

--- yacht/Yacht.fs
+++ yacht/Yacht.fs
@@ -15,30 +15,23 @@ type Category =
     | Yacht
 
 type Die =
-    | One
-    | Two
-    | Three
-    | Four
-    | Five
-    | Six
+    | One = 1
+    | Two = 2
+    | Three = 3
+    | Four = 4
+    | Five = 5
+    | Six = 6
 
-let v die : int =
-    match die with
-    | One -> 1
-    | Two -> 2
-    | Three -> 3
-    | Four -> 4
-    | Five -> 5
-    | Six -> 6
+let scoreSingle dice kind = dice |> List.filter (fun x -> x = kind) |> List.length |> (*) (int kind)
 
 let score category dice : int =
     match category with
-    | Ones -> dice |> List.filter (fun x -> x = One) |> List.length
-    | Twos -> dice |> List.filter (fun x -> x = Two) |> List.length |> (*) 2
-    | Threes -> dice |> List.filter (fun x -> x = Three) |> List.length |> (*) 3
-    | Fours -> dice |> List.filter (fun x -> x = Four) |> List.length |> (*) 4
-    | Fives -> dice |> List.filter (fun x -> x = Five) |> List.length |> (*) 5
-    | Sixes -> dice |> List.filter (fun x -> x = Six) |> List.length |> (*) 6
+    | Ones -> scoreSingle dice Die.One
+    | Twos -> scoreSingle dice Die.Two
+    | Threes -> scoreSingle dice Die.Three
+    | Fours -> scoreSingle dice Die.Four
+    | Fives -> scoreSingle dice Die.Five
+    | Sixes -> scoreSingle dice Die.Six
     | FullHouse ->
         let groups: (Die * Die list) list = dice |> List.groupBy id
 
@@ -47,28 +40,28 @@ let score category dice : int =
             if groups |> List.exists (fun (_, g) -> g.Length <> 2 && g.Length <> 3) then
                 0
             else
-                groups |> List.collect (fun (_, v) -> v) |> List.sumBy v
+                groups |> List.collect (fun (_, v) -> v) |> List.sumBy int
 
         | _ -> 0
 
     | FourOfAKind ->
         let groups = dice |> List.groupBy id
         if groups |> List.exists (fun (_, g) -> g.Length >= 4) then
-            groups |> List.filter (fun (_, g) -> g.Length >= 4) |> List.head |> fst |> v |> (*) 4
+            groups |> List.filter (fun (_, g) -> g.Length >= 4) |> List.head |> fst |> int |> (*) 4
         else
             0
 
     | LittleStraight ->
-        match dice |> List.sortBy v with
-        | [One; Two; Three; Four; Five] -> 30
+        match dice |> List.sortBy int with
+        | [Die.One; Die.Two; Die.Three; Die.Four; Die.Five] -> 30
         | _ -> 0
 
     | BigStraight ->
-        match dice |> List.sortBy v with
-        | [Two; Three; Four; Five; Six] -> 30
+        match dice |> List.sortBy int with
+        | [Die.Two; Die.Three; Die.Four; Die.Five; Die.Six] -> 30
         | _ -> 0
 
-    | Choice -> dice |> List.sumBy v
+    | Choice -> dice |> List.sumBy int
     | Yacht ->
         match dice |> List.groupBy id with
         | [(_, g)] when g.Length = 5 -> 50
