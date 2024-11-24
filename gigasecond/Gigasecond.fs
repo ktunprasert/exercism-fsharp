1a949b73a6c9dabe1e5ba412875e5b93cdce9a12
ktunprasert <ktunprasert@outlook.com>
2024-11-24 16:46:52 +0000

    gigasecond: wash

--- gigasecond/Gigasecond.fs
+++ gigasecond/Gigasecond.fs
@@ -2,7 +2,7 @@ module Gigasecond
 
 open System
 
-let GigaSecond = 1_000_000_000L
+let GigaSecond = 1_000_000_000.0
 
-let add (beginDate: DateTime): DateTime = beginDate.AddSeconds(float GigaSecond)
+let add (beginDate: DateTime): DateTime = beginDate.AddSeconds(GigaSecond)
 
